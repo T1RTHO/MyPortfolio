f20740d17a865d9326b473c3e6114b4e488eaf30
Animesh Das Chowdhury <61613419+T1RTHO@users.noreply.github.com>
2022-06-23 01:38:30 +0600

    Add files via upload

---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+/*typing animation*/
+
+var typed = new Typed(".typing",{
+	strings:["I am an undergraduate student who likes to do coding and gaming.","I am an undergraduate student who likes to do coding and gaming.","I'm very passionate and dedicated to my assigned work.","May be I am a slow learner, but I am a hard worker.","Nice to meet you!                                 "],
+	showCursor: false,
+	typeSpeed:80,
+	BackSpeed:60,
+	loop:true
+})
